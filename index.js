18ac548795563d43d02f7c5bc09320a81a8b810e
PadamSharma <bpadamsharmaty@gmail.com>
2020-11-14 02:00:15 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+randomNumber1 = Math.floor(Math.random() * 6 + 1);
+randomNumber2 = Math.floor(Math.random() * 6 + 1);
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").textContent = "Player1  Wins";
+} else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").textContent = "Player2  Wins";
+} else {
+    document.querySelector("h1").textContent = "Draw";
+}
+
+document
+    .querySelector(".img1")
+    .setAttribute(
+        "src",
+        "/var/www/html/Dicee Challenge - Starting Files/Dicee Challenge - Starting Files/images/dice" +
+            randomNumber1 +
+            ".png"
+    );
+
+document
+    .querySelector(".img2")
+    .setAttribute(
+        "src",
+        "/var/www/html/Dicee Challenge - Starting Files/Dicee Challenge - Starting Files/images/dice" +
+            randomNumber2 +
+            ".png"
+    );
